ead8582450d389c583bb38356f6a069a47317e0d
Akshay Jain <114152892+heyakshayjain@users.noreply.github.com>
2025-02-08 22:55:16 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { ThemeToggle } from "./ThemeToggle";
 import {
   NavigationMenu,
   NavigationMenuContent,
